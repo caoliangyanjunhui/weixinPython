0079a3cdd244267e3428a8bda80dc69ecdf8bb47
Gavin <wingman639@yahoo.com.cn>
2014-01-03 13:25:46 +0800

    try weixin new comer api

--- downloadServer.py
+++ downloadServer.py
@@ -10,163 +10,85 @@ import socket
 from SocketServer import ThreadingMixIn
 import threading
 import sys
-sys.path.append("../lib")
-import pathHelper
-import fileHexReading
+import hashlib
 
 
 class Handler( BaseHTTPRequestHandler ):
+	TOKEN = 'this_is_my_token'
 
 	def do_GET(self):
 		print threading.currentThread().getName()
-		if self.isPartialDownloadRequest():
-			self.partialTransfer()
-		else:
-			self.fullTransfer()
+		print self.path
+		self.getParams = self.requestGet()
+		print self.getParams
+		print self.isWeixinSignature()
+		self.sendResponse()
+
+
 		return
 		
-	def isPartialDownloadRequest(self):
-		rangeStr = self.headers.get("RANGE")
-		if not rangeStr:
-			return False
-		ifRangeStr = self.headers.get("IF-RANGE")
-		filePath = self.localFilePath( self.fileNameFromHTTPPath (self.path) )
-		zipDateTimeTag = fileHexReading.zipFileDateTimeTag( filePath )
-		print ifRangeStr.upper() + ' == ' + zipDateTimeTag.upper()
-		return ifRangeStr.upper() == zipDateTimeTag.upper()
-        
-	def requestFileOffset(self):
-		#   Range: bytes=9855420-
-		start = self.headers.get("Range")
-		print start
-		try:
-			pos = int(start[6:-1])
-		except ValueError:
-			self.send_error(400, "bad range specified.")
-			return None
-		return pos
-        
-        
-	def partialTransfer(self):
-		# Only support offset to end transfer
-		f = self.fileForDownload( self.fileNameFromHTTPPath (self.path) )
-		if f:
-			pos = self.requestFileOffset()
-			self.sendPartialDownlaodHead(pos, f)
-			f.seek(pos)
-			self.transferFile(f)
-		else:
-			self.send_error(404, "File not found")
-			print '404 File not found: ' + self.path
-		return
+	def requestGet(self):
+		paramDict = {}
+		pathParts = self.path.split('?', 1)
+		if len(pathParts) < 2: return paramDict
+		get_str = pathParts[1]
+		if not get_str: return paramDict
+		parameters = get_str.split('&')
+		for param in parameters:
+			pair = param.split('=')
+			key = pair[0]
+			value = pair[1]
+			paramDict[key] = value
+		return paramDict
+
+
+	def isWeixinSignature(self):
+		signature = self.getParams['signature']
+		timestamp = self.getParams['timestamp']
+		nonce = self.getParams['nonce']
+		echostr = self.getParams['echostr']
+		wishSignature = self.localSignature(self.TOKEN, timestamp, nonce)
+		print signature, wishSignature
+		if signature == wishSignature:
+			return True
+		return False
+
 		
-	def sendPartialDownlaodHead(self, pos, f):
-		self.send_response(206)
-		self.send_header("Content-type", 'application/octet-stream')
-		self.send_header("Connection", "keep-alive")
-		fs = os.fstat(f.fileno())
-		fullLength = fs.st_size
-		transferLength = fullLength - pos
-		print transferLength
-		self.send_header("Content-Length", str(transferLength))
-		self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
-		contentRange = "bytes %s-%s/%s" % (str(pos), fullLength-1, fullLength) #Content-Range: bytes 64312833-64657026/64657027
-		print transferLength
-		print contentRange
-		self.send_header("Content-Range", contentRange)
-		self.send_header("Cache-Control", 'must-revalidate');
-		self.end_headers()
-		return
-        
-        
-	def fullTransfer(self):
-		f = self.fileForDownload( self.fileNameFromHTTPPath (self.path) )
-		if f:
-			self.send_head(f)
-			self.transferFile(f)
-		else:
-			self.send_error(404, "File not found")
-			print '404 File not found: ' + self.path
-		return
+		
+	def sendResponse(self):
+		text = self.getParams['echostr']
+		self.send_head(text)
+		self.wfile.write(text)
+		self.wfile.write('\n')
+		self.wfile.close()
 	
 	
-	def send_head(self, f):
+	def send_head(self, text):
 		self.send_response(200)
-		self.send_header("Content-type", 'application/octet-stream')
-		fs = os.fstat(f.fileno())
-		fullLength = fs.st_size
+		self.send_header("Content-type", 'text/html')
+		
+		fullLength = len(text)
 		print fullLength
 		self.send_header("Content-Length", str(fullLength))
-		self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
 		self.send_header("Cache-Control", 'must-revalidate');
 		self.end_headers()
 		return
 
-	def localFilePath(self, fileName):
-		downloadRootPath = pathHelper.downloadRootPath()
-		if fileName.find('.zip') > 0:
-			subFolderName = 'db'
-		elif fileName.find('.ipa') > 0:
-			subFolderName = 'ipa'
-		elif fileName.find('.plist') > 0:
-			subFolderName = 'ipa'
-		elif fileName.find('.html') > 0:
-			subFolderName = 'ipa'
-		else:
-			subFolderName = 'images'
-		return os.path.join( downloadRootPath, subFolderName, fileName )
 		
-	def fileForDownload(self, fileName):
-		filePath = self.localFilePath( fileName )
-		print filePath
-		f = None
-		try:
-			f = open(filePath, 'rb')
-		except IOError:
-			return None		
-		return f
-		
-		
-	def fileNameFromHTTPPath(self, path):        
-		# abandon query parameters
-		path = path.split('?',1)[0]
-		path = path.split('#',1)[0]
-		path = posixpath.normpath(urllib.unquote(path))
-		words = path.split('/')
-		words = filter(None, words)
-		if len(words) > 0:
-			return words[-1]
-		return None
-		
-		
-	def copyfile(self, source, outputfile):
-		"""Copy all data between two file objects.
+	def localSignature(self, token, timestamp, nonce):
+		items = [token, timestamp, nonce]
+		items.sort()
+		sha1 = hashlib.sha1()
+		map(sha1.update,items)
+		hashcode = sha1.hexdigest()
+		return hashcode
 
-        The SOURCE argument is a file object open for reading
-        (or anything with a read() method) and the DESTINATION
-        argument is a file object open for writing (or
-        anything with a write() method).
+	
 
-        The only reason for overriding this would be to change
-        the block size or perhaps to replace newlines by CRLF
-        -- note however that this the default server uses this
-        to copy binary data as well.
-		"""
-		shutil.copyfileobj(source, outputfile)
-		
-	def transferFile(self, f):
-		try:
-			self.copyfile(f, self.wfile)
-			self.log_message('"%s" %s', self.requestline, "req finished.")
-		except socket.error:
-			self.log_message('"%s" %s', self.requestline, "req terminated.")
-		finally:
-			f.close()
-		return None
 		
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    """Handle requests in a separate thread."""
-    
+	"""Handle requests in a separate thread."""
+	
 if __name__ == '__main__':
 	server_address = ('', 8181) #('localhost', 8181)
 	#server = HTTPServer( server_address, Handler)
